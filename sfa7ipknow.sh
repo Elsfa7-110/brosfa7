22bae1e9ca548c093708af58d5d1807f17dccad2
Ahmed Salah Abdalhfaz <mazoka433@gmail.com>
2017-07-30 02:40:32 +0300

    Add files via upload

---
+++ sfa7ipknow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+
+
+echo "#      Elsfa7-110>>Team Zer0..       #"
+echo "######################################"
+
+echo ""
+echo ""
+echo "Checking your IP Address"
+echo "may take a seconds"
+echo ""
+echo "Your IP is: "
+echo "al ip bta3k aho: "
+curl -s checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
+echo ""
+echo "If you do not see your IP address, you may not be connected to the internet."
+echo "lw al ip mzhrsh yb2a anta 3ndk elnet f2sel ya sa7bii."
+echo "good bye."
+echo "slam ya zbii."
+
